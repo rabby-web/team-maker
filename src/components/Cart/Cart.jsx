496169db7a41f4021ea7e43ed28ca130c0a308c0
rabby-web <rabby.webdeveloper@gmail.com>
2023-09-14 15:56:15 +0600

    done project

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,15 @@
-
+import './Cart.css'
 
 // eslint-disable-next-line react/prop-types
 const Cart = ({selectedActors, remaining,totalCost}) => {
     return (
-        <div>
+        <div className='card-style'>
             <h3>Total Actors: {selectedActors.length}</h3>
+            <hr />
             <h4>Remaining: {remaining}</h4>
             <h4>TotalCost: {totalCost}</h4>
+            <hr />
+            <h3>Actor List</h3>
             {
                 // eslint-disable-next-line react/prop-types
                 selectedActors.map((actor,idx) => <li key={idx}>{actor.name}</li>)
